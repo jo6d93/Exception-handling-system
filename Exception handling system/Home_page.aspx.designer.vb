3b9decfc75ad9def90266437468858c962d39528
jo6d93 <83051457+jo6d93@users.noreply.github.com>
2025-11-10 09:48:25 +0800

    新增2

--- Exception handling system/Home_page.aspx.designer.vb
+++ Exception handling system/Home_page.aspx.designer.vb
@@ -1,10 +1,10 @@
 ﻿'------------------------------------------------------------------------------
-' <自動產生的>
+' <自動產生>
 '     這段程式碼是由工具產生的。
 '
 '     變更這個檔案可能會導致不正確的行為，而且如果已重新產生
 '     程式碼，則會遺失變更。
-' </自動產生的>
+' </自動產生>
 '------------------------------------------------------------------------------
 
 Option Strict On
@@ -12,7 +12,7 @@ Option Explicit On
 
 
 Partial Public Class Home_page
-    
+
     '''<summary>
     '''form1 控制項。
     '''</summary>
@@ -21,7 +21,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
-    
+
     '''<summary>
     '''Panel3 控制項。
     '''</summary>
@@ -30,7 +30,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Panel3 As Global.System.Web.UI.WebControls.Panel
-    
+
     '''<summary>
     '''Image1 控制項。
     '''</summary>
@@ -39,7 +39,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Image1 As Global.System.Web.UI.WebControls.Image
-    
+
     '''<summary>
     '''Label1 控制項。
     '''</summary>
@@ -48,7 +48,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''ImageButton1 控制項。
     '''</summary>
@@ -57,7 +57,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents ImageButton1 As Global.System.Web.UI.WebControls.ImageButton
-    
+
     '''<summary>
     '''ImageButton2 控制項。
     '''</summary>
@@ -66,7 +66,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents ImageButton2 As Global.System.Web.UI.WebControls.ImageButton
-    
+
     '''<summary>
     '''ImageButton3 控制項。
     '''</summary>
@@ -75,7 +75,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents ImageButton3 As Global.System.Web.UI.WebControls.ImageButton
-    
+
     '''<summary>
     '''ImageButton4 控制項。
     '''</summary>
@@ -84,7 +84,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents ImageButton4 As Global.System.Web.UI.WebControls.ImageButton
-    
+
     '''<summary>
     '''Panel2 控制項。
     '''</summary>
@@ -93,7 +93,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Panel2 As Global.System.Web.UI.WebControls.Panel
-    
+
     '''<summary>
     '''Table4 控制項。
     '''</summary>
@@ -102,7 +102,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Table4 As Global.System.Web.UI.WebControls.Table
-    
+
     '''<summary>
     '''user_name 控制項。
     '''</summary>
@@ -111,7 +111,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents user_name As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''Label3 控制項。
     '''</summary>
@@ -120,7 +120,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Label3 As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''LinkButton1 控制項。
     '''</summary>
@@ -129,7 +129,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents LinkButton1 As Global.System.Web.UI.WebControls.LinkButton
-    
+
     '''<summary>
     '''LinkButton7 控制項。
     '''</summary>
@@ -138,7 +138,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents LinkButton7 As Global.System.Web.UI.WebControls.LinkButton
-    
+
     '''<summary>
     '''LinkButton6 控制項。
     '''</summary>
@@ -147,7 +147,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents LinkButton6 As Global.System.Web.UI.WebControls.LinkButton
-    
+
     '''<summary>
     '''LinkButton8 控制項。
     '''</summary>
@@ -156,7 +156,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents LinkButton8 As Global.System.Web.UI.WebControls.LinkButton
-    
+
     '''<summary>
     '''Table5 控制項。
     '''</summary>
@@ -165,7 +165,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Table5 As Global.System.Web.UI.WebControls.Table
-    
+
     '''<summary>
     '''Label5 控制項。
     '''</summary>
@@ -174,7 +174,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Label5 As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''Panel1 控制項。
     '''</summary>
@@ -183,7 +183,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Panel1 As Global.System.Web.UI.WebControls.Panel
-    
+
     '''<summary>
     '''Table1 控制項。
     '''</summary>
@@ -192,7 +192,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Table1 As Global.System.Web.UI.WebControls.Table
-    
+
     '''<summary>
     '''Label2 控制項。
     '''</summary>
@@ -201,7 +201,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Label2 As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''DropDownList1 控制項。
     '''</summary>
@@ -210,7 +210,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents DropDownList1 As Global.System.Web.UI.WebControls.DropDownList
-    
+
     '''<summary>
     '''Panel4 控制項。
     '''</summary>
@@ -219,7 +219,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Panel4 As Global.System.Web.UI.WebControls.Panel
-    
+
     '''<summary>
     '''Label4 控制項。
     '''</summary>
@@ -228,7 +228,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Label4 As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''Label6 控制項。
     '''</summary>
@@ -237,7 +237,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Label6 As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''Label7 控制項。
     '''</summary>
@@ -246,7 +246,7 @@ Partial Public Class Home_page
     '''若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
     '''</remarks>
     Protected WithEvents Label7 As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''GridView1 控制項。
     '''</summary>
